f556c4c26bd2c26a95a96a03ee26ecdcd9debf2b
izzett222 <iizzeddin62@gmail.com>
2022-07-15 19:30:06 +0200

    bitmovin player

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import BitPlayer from './Player';
+function App() {
+  return (
+    <div className="App">
+      <div id='player-wrapper'>
+        <BitPlayer />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Player.js
@@ -0,0 +1,48 @@
+import { useEffect, useRef, useState } from 'react';
+import { UIFactory } from 'bitmovin-player/bitmovinplayer-ui';
+import { Player } from 'bitmovin-player';
+import 'bitmovin-player/bitmovinplayer-ui.css';
+
+const BitPlayer = () => {
+    const [player, setPlayer] = useState(null)
+    const playerConfig =  {
+        key: process.env.REACT_APP_KEY
+    };
+    const playerSource = {
+        dash: process.env.REACT_APP_VIDEO,
+        drm: {
+            widevine: {
+                LA_URL: process.env.REACT_APP_WIDEVINE
+            }
+
+        }
+    };
+    const playerDiv = useRef()
+    useEffect(() => {
+        setupPlayer()
+        return () => destroyPlayer()
+    }, [])
+
+    const setupPlayer = () => {
+        const player = new Player(playerDiv.current, playerConfig);
+        UIFactory.buildDefaultUI(player);
+        player.load(playerSource).then(() => {
+            setPlayer(player)
+            console.log('Successfully loaded source function');
+        }, () => {
+            console.log('Error while loading source function');
+        });
+    }
+
+    const destroyPlayer = () => {
+        if (player != null) {
+            player.destroy();
+            setPlayer(null)
+            console.log("destroyed")
+        }
+    }
+
+        return <div id='player' ref={playerDiv} />;
+}
+
+export default BitPlayer;
